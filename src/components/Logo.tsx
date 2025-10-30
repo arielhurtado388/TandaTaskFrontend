fdf984c731a187694ed9edd71cfbc2574d270d8e
Ariel Hurtado <arielhurtado388@gmail.com>
2025-10-30 10:10:45 -0500

    Logo lleva al index

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function Logo() {
-  return <img src="/logo.png" alt="Logo" />;
+  return (
+    <Link to="/">
+      <img src="/logo.png" alt="Logo" />
+    </Link>
+  );
 }
